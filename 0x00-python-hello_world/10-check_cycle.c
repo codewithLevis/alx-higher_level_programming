29b1298b7ab9453838ed1ab34df508e2de07157f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-07 01:32:04 +0300

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*check_cycle - checks for a loop/cycle in
+*singly-linked list
+*@list: list pointer
+*Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *first, *second;
+
+	if (!list)
+		return (0);
+	first = list->next;
+	second = list;
+
+	for (; first->next != NULL && first != NULL;
+	first = first->next->next, second = second->next)
+		if (first == second)
+			return (1);
+	return (0);
+}
+	
