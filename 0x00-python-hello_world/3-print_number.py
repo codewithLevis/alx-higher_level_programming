700dc8d90f61bd9cd86e672d5ca385826b61260b
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-06 17:35:56 +0300

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print('{0} {1}\n'.format(number, "Battery street"))
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print('{0} {1}'.format(number, "Battery street"))
