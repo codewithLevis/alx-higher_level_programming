ac783fcbe4521ecc2b7ac0c69a901959f44ccdbf
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-10 02:37:55 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     values = dir(hidden_4)
-    for value in value:
+    for value in values:
         if value[:2] != "__":
             print(value)
