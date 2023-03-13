1ce7cec4c7ff6d38987d73000403a60423208419
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-14 02:04:56 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 !/usr/bin/python3
 
 def element_at(my_list, idx):
+    
     length = len(my_list)
-    if idx >= 0 and idx <=(length - 1):
+    if idx >= 0 and idx <= (length - 1):
         return my_list[idx]
     else:
         return None
