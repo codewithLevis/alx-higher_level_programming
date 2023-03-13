cc13a45f2f0447f01dd014f4d23269dda7716604
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-14 02:45:36 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for raw in matrix:
         for column in range(len(raw)):
             if column < len(raw) - 1:
-                print('{}'.format(raw[column]), end=' ')
+                print('{:d}'.format(raw[column]), end=' ')
             else:
-                print('{}'.format(raw[column]), end='')
+                print('{:d}'.format(raw[column]), end='')
         print()
