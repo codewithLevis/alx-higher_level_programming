864cd38e7962a4915e3884f6da9f8c0903518eee
codewithLevis <levisomondi368@gmail.com>
2023-03-13 15:44:05 -0700

    Success

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx >= 0 and idx <= (length -1):
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    length = len(new_list)
+    if idx >= 0 and idx <=(length - 1): 
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
