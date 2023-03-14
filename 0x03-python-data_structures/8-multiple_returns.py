688c5ae1a820bc637919711583608ab7a672ff99
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-14 03:07:40 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
         first_char = None
-     else:
+    else:
         first_char = sentence[0]
     return (length, first_char)
