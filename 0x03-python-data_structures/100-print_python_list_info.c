90f71adc19dccf77b4590f6563f23bac9a6f4be2
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-15 01:54:34 +0300

    Update 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -20,7 +20,7 @@ void print_python_list_info(PyObject *p)
 	while (j < size)
 	{
 		printf("Element %d: ", j);
-		printf("%s\n", ((PyList_GetItem(p, i))->ob_type)->tp_name);
+		printf("%s\n", ((PyList_GetItem(p, j))->ob_type)->tp_name);
 		j++;
 	}
 }
