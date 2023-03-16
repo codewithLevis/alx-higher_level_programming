e4798d4d2034e51ef27d6ae199b74baabfc32ece
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-16 05:15:01 +0300

    Create 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if len(my_list) = 0:
+        return my_list
+    replaced = [idx if idx != search else replace for idx in my_list]
+    return replaced
