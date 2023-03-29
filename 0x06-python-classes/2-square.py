59eb8d22eb644e78d38033fa2a678f9bb8101e7d
codewithLevis <levisomondi368@gmail.com>
2023-03-29 16:36:20 -0700

    Classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-"""square module"""
+"""Square module"""
 
 
 class Square:
-   """function shaping a square object"""
+   """Function shaping a square object"""
     def __init__(self, size = 0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""square module"""
+"""Square module"""
 
 
 class Square:
-    """function shaping a square object"""
+    """Function shaping a square object"""
     def __init__(self, size=0):
         self.size = size
 
