36a442e8c1bdbaf30ce905ceece5df200341ebd7
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-30 02:52:32 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@
 class Square:
     """Function shaping a square object"""
     def __init__(self, size=0):
-        if isinstance(size, int) is False:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
