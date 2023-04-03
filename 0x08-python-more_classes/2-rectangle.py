323fdb8e647cc359bd7aa93f7852471397dcc7d5
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-03 23:04:13 +0300

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,8 +5,8 @@ class Rectangle:
 the width and height of a rectangle. Setters are used to ensure the values
 are positive integers'''
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
     
     @property
     def width(self):
@@ -38,6 +38,6 @@ are positive integers'''
         return(self.width * self.height)
     
     def perimeter(self):
-        if self.width = 0 or self.height = 0:
+        if self.width == 0 or self.height == 0:
             return(0)
         return(2 * (self.width + self.height)
