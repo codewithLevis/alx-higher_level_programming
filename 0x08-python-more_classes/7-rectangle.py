212df277c5911a6ec71db18b2975d18807e2cb05
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-04 21:29:08 +0300

    Update 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 class Rectangle:
     '''initialize the class attribute'''
     number_of_instances = 0
@@ -23,20 +24,45 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.width = self.__width
+        else:
+            self.__width = value
     
     @property
     def height(self):
-      '''Gets the height of the rectangle and returns its integer value'''
+        '''Gets the height of the rectangle and returns its integer value'''
         return(self.__height)
     
     @height.setter
-    '''sets the height of the rectangle'''
+    
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.height = self.__height
+        else:
+            self.__height = value
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return(0)
+        
+        return(2 * (self.__width + self.__height))
+   
+    def area(self):
+        return(self.__width * self.__height)
+    
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return("")
+        rectangle = '#'
+        for i in range(self.__width):
+            rectangle = rectangle * self.__height
+        
+            if i != (self.__width - 1):
+                print("")
+        return(rectangle)
+    
+    def __repr__(self):
+        return(f"Rectangle({self.width}, {self.height})")
