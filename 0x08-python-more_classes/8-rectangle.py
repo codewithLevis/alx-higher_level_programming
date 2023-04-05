0d6cd5d8b7b27d08f10bb811e9fcb01c8884fa7c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-05 18:43:30 +0300

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
     def area(self):
         """Returns the area of the rectangle"""
 
-        return self._width * self._height
+        return self.__width * self.__height
 
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
