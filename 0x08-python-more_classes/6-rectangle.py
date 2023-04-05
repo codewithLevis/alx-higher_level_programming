90789ced4803819f63d1bb3fce3084701fb65db0
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-05 16:41:17 +0300

    Update 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -64,5 +64,5 @@ class Rectangle:
                 rectangle += '\n'
         return(rectangle)
 
-   def __repr__(self):
-       return(f"Rectangle({self.width}, {self.height})")
+    def __repr__(self):
+        return(f"Rectangle({self.width}, {self.height})")
