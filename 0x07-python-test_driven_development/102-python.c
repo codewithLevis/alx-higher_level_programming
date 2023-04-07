52fdc11144477532cda7ff4ade1c65b5c01e4aeb
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-08 00:41:50 +0300

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,21 +9,21 @@ void print_python_string(PyObject *p)
 {
 	long int len;
 
-	fflush(stdout);
-
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
+
+	if (!PyUnicode_Check(p))
 	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
+	printf("  [ERROR] Invalid String Object\n");
+	return;
 	}
 
-	len = ((PyASCIIObject *)(p))->len;
+	len = PyUnicode_GET_LENGTH(p);
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 	printf("  type: compact ascii\n");
 	else
-	printf("  type: compact unicode object\n");
+		printf("  type: compact unicode object\n");
+	
 	printf("  length: %ld\n", len);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
 }
