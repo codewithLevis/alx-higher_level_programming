637ef77ba23f6a3b60ed884253556e2f923763cc
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-07 21:36:38 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,21 +3,24 @@
 Module printing a string and the manner
 of printing is affected by three ".?:" characters
 """
- function that prints a text with 2 new lines after each of these characters: ., ? and :
 
-def text_indentation(text):
+
+def print_indented_text(txt):
     """  function that prints a text with 2 new lines  """
+    if not isinstance(txt, str):
+        raise TypeError("txt must be a string")
 
-    if type(text) is not str:
-        raise TypeError("text must be a string")
+    i = 0
+    while i < len(txt) and txt[i] == ' ':
+        i += 1
 
-    mod_text = text[:]
-
-    for char in ".?:":
-        parsed_t = s.split(d)
-        s = ""
-        for i in parsed_t:
-            i = i.strip(" ")
-            mod_text = i + char if mod_text is "" else mod_text + "\n\n" + i + char
-
-    print(mod_text[:-3], end="")
+    while i < len(txt):
+        print(txt[i], end="")
+        if txt[i] == "\n" or txt[i] in ".?:":
+            if txt[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(txt) and txt[i] == ' ':
+                i += 1
+            continue
+        i += 1
