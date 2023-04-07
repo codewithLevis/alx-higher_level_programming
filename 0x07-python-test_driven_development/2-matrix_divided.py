f5e33f22b65fb08b88194fed6bb576109ed0a965
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-07 23:10:01 +0300

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,16 +5,16 @@
 def matrix_divided(matrix, div):
     """A function that divides all elements of a matrix By a Vector"""
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be \
-                        a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be "
+                        "a matrix (list of lists) of integers/floats")
     for i in matrix:
         if not isinstance(i, list):
-            raise TypeError("matrix must be a \
-                            matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a "
+                            "matrix (list of lists) of integers/floats")
         for j in i:
             if not isinstance(j, (int, float)):
-                raise TypeError("matrix must be a \
-                                matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a "
+                                "matrix (list of lists) of integers/floats")
     len_lis1 = len(matrix[0])
     for i in matrix:
         if len(i) > len_lis1 or len(i) < len_lis1:
