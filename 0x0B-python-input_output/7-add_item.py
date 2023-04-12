ad4224db1baabaae8b87600038e0ebaf6dcf5f46
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-12 23:45:28 +0300

    Create 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from . import save_to_json_file, load_from_json_file
+
+filename = "add_item.json"
+if not os.path.exists(filename):
+  with open(filename, 'w') as file:
+    file.write('[]')
+    
+"""Load json data from existing file, convert to python object"""
+list = load_from_json_file(filename)
+
+list += sys.argv[1:]
+
+"""saves the updated list to file"""
+
+save_to_json_file(list, filename)
