63689bcec623fcd2a79edbc537328c8e4b47db29
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 05:00:41 +0300

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
+"""File and Json"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
-  """writes an object to a text file using Json representation"""
-  with open(filename, mode="w", encoding="utf-8") as file:
-    json.dump(my_obj, file)
+    """writes an object to a text file using Json representation"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
