40b107219166236456933323d22732997d8969c9
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 04:45:27 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """File and Json"""
 
 
-def readfile(filename=""):
+def read_file(filename=""):
     """Function that opens, reads and print file content to the console"""
     with open(filename, encoding="utf-8") as f:
         filecontent = f.read()
