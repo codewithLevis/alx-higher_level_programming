de03ea633a9c1724194dec114ecef40c5cdffdf8
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 05:10:17 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,9 @@
 """File and Json"""
 import sys
 import os
-from . import save_to_json_file, load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 if not os.path.exists(filename):
