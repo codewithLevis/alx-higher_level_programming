8dc011d0aae973698f0c8741cef7d3d18b03a32f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 04:47:17 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,9 +6,4 @@ def write_file(filename="", text=""):
     '''Function writes a string to a text file 
     and returns the number of characters written'''
     with open(filename, "w", encoding='utf8') as file:
-        try:
-            file.write(text)
-            return len(text)
-        except Exception as e:
-            print("Error writing to file {}: {}".format(filename, e))
-            raise e
+        return file.write(text)
