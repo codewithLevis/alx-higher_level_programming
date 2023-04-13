7f56d8599dc9cb29426f53f4092eca83b28add1b
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-13 04:51:58 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""File and Json"""
+
 
 def append_write(filename="", text=""):
-  """appends text at the beginning of an existing file
-  creates the file and writes the text if file does not exist
-  returns the number of characters added to the file"""
+    """appends text at the beginning of an existing file
+    creates the file and writes the text if file does not exist
+    returns the number of characters added to the file"""
     with open(filename, mode = "a", encoding="utf-8") as file:
         characters = file.write(text)
-        return (characters)
+    return (characters)
