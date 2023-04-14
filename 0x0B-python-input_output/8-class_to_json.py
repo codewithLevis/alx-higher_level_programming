1de1b0fc93513f8069f96d5fd5788ae4f5032015
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-04-14 06:04:49 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" function that returns the dictionary description with simple data structure 
-(list, dictionary, string, integer and boolean) for JSON serialization of an object
+""" function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object
 """
 
 
 def class_to_json(obj):
-	"""Iterate over object attributes"""
-	return obj.__dict__			
+    """Iterate over object attributes"""
+    return obj.__dict__
