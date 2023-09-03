d26fb1d10b98e13e0db136d56eb693682ac2a293
codewithLevis <levisomondi368@gmail.com>
2023-09-03 15:11:25 -0700

    Python Networking

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Display only body of a 200 status code response
-status_code=$(curl -s -o /dev/null -w "%{http_code}" -L "$1")
-
-if [ "$status_code" == 200 ]; then
-	response=$(curl -s -L "$1")
-	echo "$response"
-fi
+curl -sL "$1"
