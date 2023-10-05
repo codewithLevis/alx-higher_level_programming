d7f3f7110f95cc240c7fa22abc30b48d1cd7558f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-10-06 01:11:41 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const fileName = process.argv[2];
-const data = process.argv[3];
-
-fs.writeFile(fileName, data, (err) => {
-  console.error(err);
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-const request = require('request');
-
-request.get(process.argv[2], (err, response, body) => {
-  if (!err && response.statusCode === 200) {
-    console.log('code: ' + body);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
-});
